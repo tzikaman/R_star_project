40d543530b118c9b7eb4c7792129784803068993
REsurvivor <47045430+REsurvivor@users.noreply.github.com>
2024-09-15 21:43:18 +0300

    Added osm reader Class.

---
+++ osm_reader.py
@@ -0,0 +1,23 @@
+import osmium
+
+class GetNamesAndLocs(osmium.SimpleHandler):
+    
+    def __init__(self):
+        osmium.SimpleHandler.__init__(self)
+        self.ids = []
+        self.lats = []
+        self.lons = []
+        
+    def node(self, n):
+        self.ids.append(n.id)
+        self.lats.append(n.location.lat)
+        self.lons.append(n.location.lon)
+
+
+
+
+# h = GetNamesAndLocs()
+# h.apply_file('C:\\Users\\User\\Documents++\\university\\'
+#              'subjects\\databases_technologies\\\project\\map.osm')
+
+# print(f"{len(h.ids)}\n{len(h.lats)}\n{len(h.lons)}")
